afccac55585468fe9d2c83e9d14f8f0d66912e30
wyspvz2 <68979551+wyspvz2@users.noreply.github.com>
2025-10-02 10:17:21 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,1 +1,22 @@
 
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+
+def get_dataloaders(batch_size=64):
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,))
+    ])
+
+    train_dataset = torchvision.datasets.MNIST(
+        root="./data", train=True, transform=transform, download=True
+    )
+    test_dataset = torchvision.datasets.MNIST(
+        root="./data", train=False, transform=transform, download=True
+    )
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    return train_loader, test_loader
